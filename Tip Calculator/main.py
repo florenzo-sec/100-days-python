a2daf9741c68f2d158c64bff8fc11a49370c7009
Lorenzo Facchinetti <lorenzo.cybersec@gmail.com>
2025-10-20 21:04:42 +0200

    Delete Day 2 Tip Calculator directory for contributions bug

--- Tip Calculator/main.py
+++
@@ -1,7 +0,0 @@
-
-print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill? $"))
-tip_percentage = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
-people = int(input("How many people to split the bill? "))
-bill_tot_person = bill / people + (bill * tip_percentage / 100) / people
-print(f"Each person should pay: ${bill_tot_person}")
